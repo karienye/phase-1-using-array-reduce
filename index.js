aed61513403f5bd0ee8f57009d0f799f524cad3c
leah karienye <karienyeleah@gmail.com>
2022-06-14 12:41:37 +0300

    commit tree 732aaee2d368566119481f6b5a6a080e91ff75bb

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce
+(function(total, batches)
+{
+  return batches + total
+  }
+) 
+/*
 function totalBatteries(batteryBatches) {
     let total = 'number';
-  
-  
+    
     for (const product of batteryBatches) {
       total += batteryBatches.sum;
     };
@@ -12,4 +18,4 @@ function totalBatteries(batteryBatches) {
     return total;
   }
   
-  console.log(totalBatteries(batteryBatches)); 
+  console.log(totalBatteries(batteryBatches)); */
